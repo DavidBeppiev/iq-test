d828e74ed22e0e1545a8fe6b95c67eb89773851f
David Beppiev <pink.elephant636@gmail.com>
2022-10-07 22:34:27 +0300

    === FIX === 1) fix some sizes and layout

--- lib/ui/pages/tests_description_page.dart
+++ lib/ui/pages/tests_description_page.dart
@@ -9,52 +9,54 @@ class TestsDescriptionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
+        body: SingleChildScrollView(
+          child: SafeArea(
       child: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 25.0, right: 20.0, left: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(child: IconButton(onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back))),
-                      const Expanded(
-                        flex: 5,
-                        child: Text(
-                          MyStrings.testTitle,
-                          style: MyStyles.ts_FS35_FWbold_CText_LS7_FFPoiretOne,
-                          textAlign: TextAlign.center,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25.0, right: 20.0, left: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(child: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back))),
+                        const Expanded(
+                          flex: 5,
+                          child: Text(
+                            MyStrings.testTitle,
+                            style: MyStyles.ts_FS35_FWbold_CText_LS7_FFPoiretOne,
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                      const Expanded(child: SizedBox()),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 50.0,
-                  ),
-                  const Text(
-                    MyStrings.testDescriptionText1,
-                    style: MyStyles.ts_FS25_FWbold_CText_LS1_FFPoiretOne,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const Text(
-                    MyStrings.testDescriptionText2,
-                    style: MyStyles.ts_FS18_FWbold_CDavys_Grey_LS1_FFPoiretOne,
-                  ),
-                  const SizedBox(height: 8.0,),
-                  CustomButton(MyStrings.moveToTestText, () => null)
-                ],
-              ),
-            ],
+                        const Expanded(child: SizedBox()),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 50.0,
+                    ),
+                    const Text(
+                      MyStrings.testDescriptionText1,
+                      style: MyStyles.ts_FS25_FWbold_CText_LS1_FFPoiretOne,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text(
+                      MyStrings.testDescriptionText2,
+                      style: MyStyles.ts_FS18_FWbold_CDavys_Grey_LS1_FFPoiretOne,
+                    ),
+                    const SizedBox(height: 8.0,),
+                    CustomButton(MyStrings.moveToTestText, () => null)
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
       ),
-    ));
+    ),
+        ));
   }
 }
--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -6,7 +6,7 @@ Widget CustomButton(String text, Function() fun) {
    return Container(
      margin: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
      width: double.infinity,
-     height: 50.0,
+     height: 59.0,
      child: ElevatedButton(
          style: ButtonStyle(
              backgroundColor: MaterialStateProperty.all(MyColors.secondaryColor),
@@ -18,6 +18,7 @@ Widget CustomButton(String text, Function() fun) {
          child: Text(
            text,
            style: MyStyles.ts_FS22_FWbold_CWhite_LS1_FFPoiretOne,
+           textAlign: TextAlign.center,
          )),
    );
 }
