62a377fac0c23f355e6e9342e760fc1162c6bcd3
DavidBeppiev <pink.elephant636@gmail.com>
2022-11-08 23:13:21 +0300

    === NEW === 1) Add app icons

--- lib/ui/pages/test_page.dart
+++ lib/ui/pages/test_page.dart
@@ -44,127 +44,124 @@ class _TestPageState extends State<TestPage> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () => _previousPage(),
-      child: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: SafeArea(
-          child: Scaffold(
-              body: Column(
-            children: [
-              BlocConsumer<TimerCubit, TimerState>(
-                listener: (context, timeState) {
-                  if (timeState is TimerFinish) {
-                    BlocProvider.of<TimerCubit>(context).resetTimer();
-                    BlocProvider.of<TestCubit>(context).calculateResult();
-                  }
-                },
-                builder: (context, timeState) {
-                  if (timeState is TimerInitial) {
-                    String strDigits(int n) => n.toString().padLeft(2, '0');
-                    final minutes = strDigits(timeState.duration!.inMinutes.remainder(60));
-                    final seconds = strDigits(timeState.duration!.inSeconds.remainder(60));
-                    return Padding(
-                      padding: const EdgeInsets.only(top: 14.0, right: 20.0, left: 14.0),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            right: 0.0,
-                            top: 4.5,
-                            child: Text(
-                              '$_currentQuestion/${questions.length}',
-                              style: MyStyles.ts_FS25_CDavyc_Grey_lS17_FFNumbers,
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              '$minutes:$seconds',
-                              style: MyStyles.ts_FS32_CDavyc_Grey_lS17_FFNumbers,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 0.0,
-                            left: 0.0,
-                            child: IconButton(
-                                splashRadius: 19.0,
-                                onPressed: _previousPage,
-                                icon: const Icon(Icons.arrow_back_ios_new)),
-                          )
-                        ],
-                      ),
-                    );
-                  }
-                  return const SizedBox();
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Divider(
-                  thickness: 1.8,
-                ),
-              ),
-              BlocConsumer<TestCubit, TestState>(listener: (context, testsState) {
-                if (testsState is TestsResult) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => TestsResultPage(
-                                countRight: testsState.countRight,
-                                countWrong: testsState.countWrong,
-                                result: testsState.result,
-                                testMeaningColor: testsState.testMeaningColor,
-                                testMeaningText: testsState.testMeaningText,
-                              )));
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+          body: SafeArea(
+            child: Column(
+        children: [
+            BlocConsumer<TimerCubit, TimerState>(
+              listener: (context, timeState) {
+                if (timeState is TimerFinish) {
+                  BlocProvider.of<TimerCubit>(context).resetTimer();
+                  BlocProvider.of<TestCubit>(context).calculateResult();
                 }
-              }, builder: (context, testsState) {
-                if (testsState is TestsLoaded) {
-                  questions = testsState.tests.data![0].questions!;
-                  return Expanded(
-                    child: PageView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        controller: pageController,
-                        itemCount: questions.length,
-                        onPageChanged: (int index) {
-                          setState(() => _currentQuestion = index + 1);
-                        },
-                        itemBuilder: (context, i) {
-                          return TestView(questions[i]);
-                        }),
+              },
+              builder: (context, timeState) {
+                if (timeState is TimerInitial) {
+                  String strDigits(int n) => n.toString().padLeft(2, '0');
+                  final minutes = strDigits(timeState.duration!.inMinutes.remainder(60));
+                  final seconds = strDigits(timeState.duration!.inSeconds.remainder(60));
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 14.0, right: 20.0, left: 14.0),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 0.0,
+                          top: 4.5,
+                          child: Text(
+                            '$_currentQuestion/${questions.length}',
+                            style: MyStyles.ts_FS25_CDavyc_Grey_lS17_FFNumbers,
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            '$minutes:$seconds',
+                            style: MyStyles.ts_FS32_CDavyc_Grey_lS17_FFNumbers,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          child: IconButton(
+                              splashRadius: 19.0,
+                              onPressed: _previousPage,
+                              icon: const Icon(Icons.arrow_back_ios_new)),
+                        )
+                      ],
+                    ),
                   );
                 }
                 return const SizedBox();
-              }),
-              Padding(
-                padding: const EdgeInsets.only(top: 5.0, right: 10.0, bottom: 25.0, left: 20.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: TextFormField(
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 10.0),
-                          hintText: MyStrings.answerText,
-                          enabledBorder: _outLineBorder,
-                          focusedBorder: _outLineBorder),
-                      cursorColor: MyColors.textColor,
-                      controller: _answerController,
-                      style: MyStyles.ts_FS22_FWbold_CText_LS1_FFPoiretOne,
-                    )),
-                    IconButton(
-                        onPressed: () => _nextPage(_answerController.text),
-                        splashRadius: 25.0,
-                        icon: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: MyColors.davysGreyColor,
-                        ))
-                  ],
-                ),
-              )
-            ],
-          )),
-        ),
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Divider(
+                thickness: 1.8,
+              ),
+            ),
+            BlocConsumer<TestCubit, TestState>(listener: (context, testsState) {
+              if (testsState is TestsResult) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TestsResultPage(
+                              countRight: testsState.countRight,
+                              countWrong: testsState.countWrong,
+                              result: testsState.result,
+                              testMeaningColor: testsState.testMeaningColor,
+                              testMeaningText: testsState.testMeaningText,
+                            )));
+              }
+            }, builder: (context, testsState) {
+              if (testsState is TestsLoaded) {
+                questions = testsState.tests.data![0].questions!;
+                return Expanded(
+                  child: PageView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      controller: pageController,
+                      itemCount: questions.length,
+                      onPageChanged: (int index) {
+                        setState(() => _currentQuestion = index + 1);
+                      },
+                      itemBuilder: (context, i) {
+                        return TestView(questions[i]);
+                      }),
+                );
+              }
+              return const SizedBox();
+            }),
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0, right: 10.0, bottom: 25.0, left: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 10.0),
+                        hintText: MyStrings.answerText,
+                        enabledBorder: _outLineBorder,
+                        focusedBorder: _outLineBorder),
+                    cursorColor: MyColors.textColor,
+                    controller: _answerController,
+                    style: MyStyles.ts_FS22_FWbold_CText_LS1_FFPoiretOne,
+                  )),
+                  IconButton(
+                      onPressed: () => _nextPage(_answerController.text),
+                      splashRadius: 25.0,
+                      icon: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: MyColors.davysGreyColor,
+                      ))
+                ],
+              ),
+            )
+        ],
       ),
+          )),
     );
   }
 
